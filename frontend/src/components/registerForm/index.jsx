c6788072d5f0952f235216ea5b2b91d6d96281ba
dscastillol <38595705+dscastillol@users.noreply.github.com>
2020-04-19 19:42:00 -0500

    Fixed some issues

--- frontend/src/components/registerForm/index.jsx
+++ frontend/src/components/registerForm/index.jsx
@@ -240,9 +240,23 @@ constructor(props){
                     onChange={this.handleChange}
 
             />
-          </div> : null}
-          
+          </div>  : null}
 
+          {isDriver ? 
+          <div class="form-group" >
+            <label  className = {styles.input}>Telefono:</label>
+            <input  type="text"
+                    name="phone"
+                    class="form-control"
+                    placeholder = "TELEFONO"
+                    //value={this.state.apellido}
+                    id='telefono'
+                    onChange={this.handleChange}
+
+            />
+          </div>: null}
+          
+          
           <div class="form-group">
             <label  className = {styles.input}>E-Mail:</label>
             <input  type="text"
@@ -324,7 +338,7 @@ constructor(props){
 
           <div class="col-md-12 text-center">
             
-            <a {...goToDriver ? {href:"/api/driver/home"} : {href:"/api/user/home"}} className={classNames("btn btn-dark")} > INGRESAR</a>
+            <a {...goToDriver ? {href:"/driver/home"} : {href:"user/home"}} className={classNames("btn btn-dark")} > INGRESAR</a>
           </div>
 
           <label className = {styles.label}>
@@ -335,7 +349,7 @@ constructor(props){
   
           <div class="col-md-12 text-center">
           
-            <a {...goToDriver ? {href:"/api/driver/register"} : {href:"/api/user/register"}} className={classNames("btn btn-dark")}>REGISTRARSE</a>
+            <a {...goToDriver ? {href:"driver/signup"} : {href:"/user/signup"}} className={classNames("btn btn-dark")}>REGISTRARSE</a>
           </div>
           
       </form>
